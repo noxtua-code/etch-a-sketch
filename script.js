82ba7a1e8ead0a228c5eb8a536a5e0f037c7bfc7
noxtua-code <[200~93567294+noxtua-code@users.noreply.github.com>
2023-01-01 19:02:40 +1000

    Added and stylised buttons and header. Added basic functionality to the grid with a mouseenter event listener

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ for (let i = 0; i < 625; i++) {
     let blankGridSquare = document.createElement('div');
     blankGridSquare.className = 'blank-grid-square';
     gridContainer.appendChild(blankGridSquare);
-}
+};
+
+
+let blankGridSquares = document.querySelectorAll('.blank-grid-square');
+
+blankGridSquares.forEach(square => {
+    square.addEventListener('mouseenter', () => {
+        square.className = 'colour-grid-square';
+    })
+});
