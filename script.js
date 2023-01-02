a458e934acfec9d6fc92bb4c45ab3274cb29c4fc
noxtua-code <[200~93567294+noxtua-code@users.noreply.github.com>
2023-01-02 12:09:25 +1000

    Added button functionality - allowing for the user to change colours and modes (such as darker/rainbow/eraser modes). I also added the ‘clear’ canvas option. Buttons swap between ‘activated/deactivated’ upon click. Clicking the colour-picker will also now active the custom button

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const gridContainer = document.querySelector('.grid-container');
+const sideButtons = document.querySelectorAll('button');
+const colorPicker = document.querySelector('#colour-picker');
+let pixelColour = '#000000';
+let activatedButton = 'black';
 
-// Creation of default etch-a-sketch canvas
+
+/**
+ * Creation and modifcation of etch-a-sketch canvas
+ */
+
 for (let i = 0; i < 625; i++) {
     let blankGridSquare = document.createElement('div');
     blankGridSquare.className = 'blank-grid-square';
@@ -8,29 +16,81 @@ for (let i = 0; i < 625; i++) {
 };
 
 
+/**
+ * Colour grid squares on mouseover
+ */
+
 let blankGridSquares = document.querySelectorAll('.blank-grid-square');
 
 blankGridSquares.forEach(square => {
-    square.addEventListener('mouseenter', (element) => {
-        square.className = 'colour-grid-square';
-
-        // Testing for incrementally darker button
-        let squareColor = window.getComputedStyle(square).getPropertyValue('background-color')
-        let rgbSquareColor = squareColor.match(/\d+/g);
-        let darkerSquareColor = `rgb(${+rgbSquareColor[0] * 0.75}, ${+rgbSquareColor[1] * 0.75}, ${+rgbSquareColor[2] * 0.75})`
-        console.log(darkerSquareColor)
+    square.addEventListener('mouseenter', () => {
         
+        switch (activatedButton) {
+            case 'black':
+                pixelColour = 'rgb(0, 0, 0)';
+                break;
+            case 'rainbow':
+                pixelColour = `rgb(${returnRandomRGBValue()}, ${returnRandomRGBValue()}, ${returnRandomRGBValue()})`
+                break;
+            case 'darker':
+                pixelColour = returnDarkerColour(square);
+                break;
+            case 'custom':
+                pixelColour = document.querySelector('#colour-picker').value;
+                break;
+            default:
+                pixelColour = '#ffffff';
+                break;
+        }
+
+        square.style.backgroundColor = pixelColour;
     })
 });
 
-blackBtn = document.querySelector('#black')
-rainbowBtn = document.querySelector('#rainbow')
-darkerBtn = document.querySelector('#darker')
-customBtn = document.querySelector('#custom')
-eraserBtn = document.querySelector('#eraser')
-clearBtn = document.querySelector('#clear')
+function returnDarkerColour(square) {
+    let squareColor = window.getComputedStyle(square).getPropertyValue('background-color');
+    let rgbSquareColor = squareColor.match(/\d+/g);
+    return `rgb(${+rgbSquareColor[0] * 0.90}, ${+rgbSquareColor[1] * 0.90}, ${+rgbSquareColor[2] * 0.90})`
+};
 
-// if active, then inactive, else active. change css colour to new colour
-// figure out how to get colour picker - 
-// eraser - remove hover tag?  or back to white with border? 
+function returnRandomRGBValue() {
+    return Math.floor(Math.random() * 256);
+}
+
+
+/**
+ * Sidebar Buttons functionality (active/deactive/colour picker/clear)
+ */
+
+function disableBtns() {
+    sideButtons.forEach(btn => {
+        btn.className = 'inactive'
+    })
+}
+
+function activateCurrentBtn(btn) {
+        btn.className = 'active';
+        activatedButton = btn.id;
+}
+
+sideButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if (btn.id === 'clear') {
+            blankGridSquares.forEach(square => {
+                square.style.backgroundColor = '#ffffff';
+            })
+        } else if (btn.className !== 'active') {
+                disableBtns();
+                activateCurrentBtn(btn);
+        };
+    })
+});
+
+colorPicker.addEventListener('click', () => {
+    activatedButton = 'custom';
+    if (sideButtons[3].className !== 'active') {
+        disableBtns();
+        sideButtons[3].className = 'active';
+    }
+})
 
